2c3c7fa1af631aacd58ffed1c9ac78b48c35329c
John Backus <johncbackus@gmail.com>
2021-08-22 17:39:33 -0400

    Split files

--- src/app.tsx
+++ src/app.tsx
@@ -2,29 +2,6 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import "./app.css";
 
-const Greet = () => {
-  return <h1>Hello, world!</h1>;
+export const App = () => {
+  return <h1>HOWDY, world!</h1>;
 };
-
-const mount = () => {
-  const root = document.querySelector("#root");
-
-  if (!root) {
-    throw new Error(
-      "Expected to find an element with the ID 'root' to mount app"
-    );
-  }
-
-  ReactDOM.render(
-    <React.StrictMode>
-      <Greet />
-    </React.StrictMode>,
-    root
-  );
-};
-
-const onLoad = (callback: () => void) => {
-  window.addEventListener("load", callback, { once: true });
-};
-
-onLoad(mount);
---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { App } from "./app";
+
+const mount = () => {
+  const root = document.querySelector("#root");
+
+  if (!root) {
+    throw new Error(
+      "Expected to find an element with the ID 'root' to mount app"
+    );
+  }
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    root
+  );
+};
+
+const onLoad = (callback: () => void) => {
+  window.addEventListener("load", callback, { once: true });
+};
+
+onLoad(mount);

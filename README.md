e6d11a10bd0682e092cdc1c427e7bc419c0b0d86
John Backus <johncbackus@gmail.com>
2021-08-22 17:26:52 -0400

    Add basic TS build

---
+++ README.md
@@ -0,0 +1,7 @@
+# Simple esbuild app
+
+## Building
+
+Run `bin/watch` to run esbuild and watch for changes on the file system.
+
+The `index.html` file is not part of esbuild but will be copied over when `bin/watch` is run.
---
+++ src/app.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const Greet = () => {
+  return <h1>Hello, world!</h1>;
+};
+
+const mount = () => {
+  const root = document.querySelector("#root");
+
+  if (!root) {
+    throw new Error(
+      "Expected to find an element with the ID 'root' to mount app"
+    );
+  }
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <Greet />
+    </React.StrictMode>,
+    root
+  );
+};
+
+const onLoad = (callback: () => void) => {
+  window.addEventListener("load", callback, { once: true });
+};
+
+onLoad(mount);
